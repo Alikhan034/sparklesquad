4819e6286830208734b53fb14a5bf68ab89a187e
Alikhan034 <alikhan809212@gmail.com>
2024-03-17 19:36:04 +0500

    home screen header

--- App/Navigations/TabNavigations.jsx
+++ App/Navigations/TabNavigations.jsx
@@ -4,14 +4,35 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../Screens/HomeScreen/HomeScreen';
 import BookingScreen from '../Screens/BookingScreen/BookingScreen';
 import ProfileScreen from '../Screens/ProfileScreen/ProfileScreen';
+import Color from '../utills/Color';
+import { Feather } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 export default function TabNavigations() {
   return (
-    <Tab.Navigator>
-        <Tab.Screen name='Home' component={HomeScreen} />
-        <Tab.Screen name='Booking' component={BookingScreen} />
-        <Tab.Screen name='Profile' component={ProfileScreen} />
+    <Tab.Navigator screenOptions={{headerShown:false,
+    tabBarActiveTintColor:Color.PRIMARY
+    }}>
+        <Tab.Screen name='home' component={HomeScreen}
+        options={{tabBarLabel:({Color})=>(
+            <Text style={{color:Color,fontSize:12,marginTop:-7}}>Home</Text>
+        ),tabBarIcon:({color,size})=>(<Feather name="home" size={size} color={color} />)
+    
+    }}
+        />
+        <Tab.Screen name='booking' component={BookingScreen}   options={{tabBarLabel:({Color})=>(
+            <Text style={{color:Color,fontSize:12,marginTop:-7}}>Booking</Text>
+        ),tabBarIcon:({color,size})=>(<FontAwesome name="bookmark-o" size={size} color={color} />)
+    
+    }}/>
+        <Tab.Screen name='profile' component={ProfileScreen}  
+         options={{tabBarLabel:({Color})=>(
+            <Text style={{color:Color,fontSize:12,marginTop:-7}}>Profile</Text>)
+        ,tabBarIcon:({color,size})=>(<FontAwesome name="user-circle" size={size} color={color} />)
+    
+    }}/>
 
 
     </Tab.Navigator>
